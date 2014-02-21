3d5bb06489211d185f60e2e257da2eca6a8a2f17
Ubuntu <ubuntu@assets.(none)>
2014-02-21 19:27:13 +0000

    Prettified!

--- CECSAM/admin.py
+++ CECSAM/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from CECSAM.models import Asset, Location, Building
 # Register your models here.
-admin.site.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('tag', 'description', 'location', 'found', 'official', 'date')
+    list_filter = ['date']
+    search_fields = ['tag']
+
+class BuildingAdmin(admin.ModelAdmin):
+    # ...
+    list_display = ('name', 'longName')
+    search_fields = ['name']
+
+admin.site.register(Asset, AssetAdmin)
 admin.site.register(Location)
-admin.site.register(Building)
+admin.site.register(Building, BuildingAdmin)
